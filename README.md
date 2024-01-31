29dd43ea5a4f182a3bebd037f7a05577b4a33eeb
Behindtheboard <150493510+Behindtheboard@users.noreply.github.com>
2024-01-31 09:28:56 -0800

    cleared readme.md will com back

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # rock-paper-scissors
 
-Pseudocode
-Prompt for user input with rock paper scissors message
-declare variable "playerSelection" with user input converted to lower case
-declare variable "computerSelection" with getComputerChoice function
--- script.js
+++ script.js
@@ -16,8 +16,10 @@ function playGame() {
     let computerScore = 0;
     let win = "You win!"
     let lose = "You lose..."
-    let draw = "Draw"
+    let draw = "Draw."
     
+    let playerName = prompt(`Welcome! Let's play some rock, paper, scissors - best of 5! \n\nWhat do we call you?`);
+
     for ( let round = 0; round < 5 ; round++) {
         function playRound(playerSelection, computerSelection) {
             let playerSelLowCase = playerSelection.toLowerCase();
@@ -35,11 +37,13 @@ function playGame() {
             }
         }
     
-        const playerSelection = prompt("rock, paper, or scissors?");
+        //let instructions = `Type r for rock\n\n p for paper\n\n or s for scissors`;
+        
+        const playerSelection = prompt(`rock, paper, or scissors?`);
         const computerSelection = getComputerChoice();
-        let singleRoundPair = `You: ${playerSelection}. Computer: ${computerSelection}.`
+        let singleRoundPair = `${playerName}: ${playerSelection}. Computer: ${computerSelection}.`
+        
         let roundResult = playRound(playerSelection, computerSelection);
-        alert(`${singleRoundPair} ${roundResult}`);
         
         if (roundResult === win) {
             playerScore += 1;
@@ -47,9 +51,10 @@ function playGame() {
             computerScore += 1;
         } else {
         }
-
-        let gameScore = `You: ${playerScore} | Computer: ${computerScore}`;
-        alert(gameScore);
+        
+        let gameScore = `${playerName}: ${playerScore} | Computer: ${computerScore}`;
+        
+        alert(`${singleRoundPair} ${roundResult}\n\nScore: ${gameScore}`);
     }
     if (playerScore > computerScore) {
         alert(`${win} The WHOLE game`);
