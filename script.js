701757e37b13298070795a494817372702072388
Behindtheboard <150493510+Behindtheboard@users.noreply.github.com>
2024-01-30 23:51:19 -0800

    update both round and computer algo

--- script.js
+++ script.js
@@ -1,35 +1,42 @@
 function getComputerChoice() {
     let randInt = Math.floor(Math.random() * 9);
     let randNumToThree = randInt % 3;
+    let rock = "rock"
+    let paper = "paper"
+    let scissors = "scissors"
     if (randNumToThree === 0) {
-        const computerChoice = "rock"
-        return computerChoice 
+        return rock;
     } else if (randNumToThree === 1) {
-        const computerChoice = "paper"
-        return computerChoice
+        return paper;
     } else if (randNumToThree === 2) {
-        const computerChoice = "scissors"
-        return computerChoice
+        return scissors;
     }
 }
 
-function playRound(playerSelection, computerSelection) {
-    let playerSelLowCase = playerSelection.toLowerCase();
-    let singleRoundScore = `You: ${playerSelLowCase}. Computer: ${computerSelection}.`
-    
-    if (playerSelLowCase === "rock" && computerSelection === "scissors") {
-        return alert(`${singleRoundScore}. You win!`)
-    } else if (playerSelLowCase === "paper" && computerSelection === "rock") {
-        return alert(`${singleRoundScore}. You win!`)
-    } else if (playerSelLowCase === "scissor" && computerSelection === "paper") {
-        return alert(`${singleRoundScore}. You win!`)
-    } else if (playerSelLowCase === computerSelection) {
-        return alert(`${singleRoundScore}. Draw!`)
-    } else {
-        return alert(`${singleRoundScore}. You Lose...`)
+function playGame() {
+    function playRound(playerSelection, computerSelection) {
+        let playerSelLowCase = playerSelection.toLowerCase();
+        let win = "You win!"
+        let lose = "You lose..."
+        let draw = "Draw"
+        if (playerSelLowCase === "rock" && computerSelection === "scissors") {
+            return win;
+        } else if (playerSelLowCase === "paper" && computerSelection === "rock") {
+            return win;
+        } else if (playerSelLowCase === "scissor" && computerSelection === "paper") {
+            return win;
+        } else if (playerSelLowCase === computerSelection) {
+            return draw;
+        } else {
+            return lose;
+        }
     }
-  }
-  
-  const playerSelection = prompt("rock, paper, or scissors?");
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
+    
+    const playerSelection = prompt("rock, paper, or scissors?");
+    const computerSelection = getComputerChoice();
+    let singleRoundPair = `You: ${playerSelection}. Computer: ${computerSelection}.`
+    let roundResult = playRound(playerSelection, computerSelection);
+    alert(`${singleRoundPair} ${roundResult}`);
+}
+
+playGame()
