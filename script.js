377ff91f0d43c9ddf07a3c03648a3a4f7f7f6126
Jason <150493510+Behindtheboard@users.noreply.github.com>
2024-02-26 18:26:47 -0800

    Integrate DOM onto buttons

--- script.js
+++ script.js
@@ -11,51 +11,66 @@ function getComputerChoice() {
     }
 }
 
-function playGame() {
-    let playerScore = 0;
-    let computerScore = 0;
-    let win = "You win!"
-    let lose = "You lose..."
-    let draw = "Draw."
-    
-    let playerName = prompt(`Welcome! Let's play some rock, paper, scissors - best of 5! \n\nWhat do we call you?`);
+let playerSelection = '';
 
-    for ( let round = 0; round < 5 ; round++) {
-        function playRound(playerSelection, computerSelection) {
-            let playerSelLowCase = playerSelection.toLowerCase();
-            
-            if (playerSelLowCase === "rock" && computerSelection === "scissors") {
-                return win;
-            } else if (playerSelLowCase === "paper" && computerSelection === "rock") {
-                return win;
-            } else if (playerSelLowCase === "scissor" && computerSelection === "paper") {
-                return win;
-            } else if (playerSelLowCase === computerSelection) {
-                return draw;
-            } else {
-                return lose;
-            }
-        }
-    
-        //let instructions = `Type r for rock\n\n p for paper\n\n or s for scissors`;
-        
-        const playerSelection = prompt(`rock, paper, or scissors?`);
-        const computerSelection = getComputerChoice();
-        let singleRoundPair = `${playerName}: ${playerSelection}. Computer: ${computerSelection}.`
-        
-        let roundResult = playRound(playerSelection, computerSelection);
-        
-        if (roundResult === win) {
-            playerScore += 1;
-        } else if (roundResult === lose) {
-            computerScore += 1;
-        } else {
-        }
-        
-        let gameScore = `${playerName}: ${playerScore} | Computer: ${computerScore}`;
-        
-        alert(`${singleRoundPair} ${roundResult}\n\nScore: ${gameScore}`);
+let fullGame = document.querySelector('#choices');
+fullGame.addEventListener('click', (event) => {
+    let target = event.target;
+    switch(target.id) {
+        case 'rock':
+            playerSelection = 'rock';
+
+            break; 
+        case 'paper':
+            playerSelection = 'paper';
+
+            break;
+        case 'scissors':
+            playerSelection = 'scissors';
+
+            break;
     }
+});
+
+
+function playGame() {
+    let win = "You win!";
+    let lose = "You lose...";
+    let draw = "Draw.";
+    
+
+    function playRound(playerSelection, computerSelection) {
+                    
+        if (playerSelection === "rock" && computerSelection === "scissors") {
+            return win;
+        } else if (playerSelection === "paper" && computerSelection === "rock") {
+            return win;
+        } else if (playerSelection === "scissor" && computerSelection === "paper") {
+            return win;
+        } else if (playerSelection === computerSelection) {
+            return draw;
+        } else {
+            return lose;
+        }
+    }
+    
+    let playerName = 'you';
+    let computerSelection = getComputerChoice();
+    let singleRoundPair = `${playerName}: ${playerSelection}. Computer: ${computerSelection}.`;
+    
+    let roundResult = playRound(playerSelection, computerSelection);
+    
+    if (roundResult === win) {
+        playerScore += 1;
+    } else if (roundResult === lose) {
+        computerScore += 1;
+    } else {
+    }
+    
+    let gameScore = `${playerName}: ${playerScore} | Computer: ${computerScore}`;
+    
+    alert(`${singleRoundPair} ${roundResult}\n\nScore: ${gameScore}`);
+    
     if (playerScore > computerScore) {
         alert(`${win} The WHOLE game`);
     } else if (playerScore === computerScore) {
@@ -63,6 +78,6 @@ function playGame() {
     } else {
         alert(`${lose} The WHOLE game.`);
     }
+    
 }
-
-playGame()
+    
